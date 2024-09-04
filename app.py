98bee915fc0c40ada2a792012f9e0a472cb8b11c
Иван Осягин <ayecantry@gmail.com>
2024-09-04 17:38:19 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,12 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-
-def start():
+@app.route('/web')
+def web():
     return """<!doctype html>
         <html>
             <body>
                 <h1>web-server on flask</h1>
+                <a href="/author">author</a>
+            <body>
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Осягин Иван Дмитриевич"
+    group = "FBI-22"
+    faculty = "FB"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
             <body>
         </html>"""
