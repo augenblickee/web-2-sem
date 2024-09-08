709b0450ca98c53642c784a7961ecff13e5cecc9
Иван Осягин <ayecantry@gmail.com>
2024-09-08 17:24:25 +0700

    Стили

--- app.py
+++ app.py
@@ -33,9 +33,13 @@ def author():
 @app.route('/lab1/oak')
 def oak():
     path = url_for('static', filename='oak.jpg')
+    css = url_for('static', filename='lab1.css')
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="''' + css + '''">
+    </head>
     <body>
         <h1>Дуб</h1>
         <img src="''' + path + '''">
