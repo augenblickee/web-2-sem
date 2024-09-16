203ee3f911c81939636e62c6e78e331c810f5b0e
Иван Осягин <ayecantry@gmail.com>
2024-09-16 21:56:46 +0700

    испраление адресов

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ from flask import Flask, url_for, redirect
 
 app = Flask(__name__)
 
-@app.route('/web')
+@app.route('/lab1/web')
 def web():
     return """<!doctype html>
         <html>
             <body>
                 <h1>web-server on flask</h1>
-                <a href="/author">author</a>
+                <a href="/lab1/author">author</a>
             <body>
         </html>""", 200, {
             "X-Server": "sample",
             'Content-Type': 'text/plain; charset=utf-8'}
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Осягин Иван Дмитриевич"
     group = "FBI-22"
@@ -26,7 +26,7 @@ def author():
                 <p>Студент: """ + name + """</p>
                 <p>Группа: """ + group + """</p>
                 <p>Факультет: """ + faculty + """</p>
-                <a href="/web">web</a>
+                <a href="/lab1/web">web</a>
             <body>
         </html>"""
 
@@ -62,9 +62,9 @@ def counter():
 </html>
 '''  
 
-@app.route('/info')
+@app.route('/lab1/info')
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route('/lab1/created')
 def created():
