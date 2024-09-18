fb65e0425f9c7fda289434d4c8c0bef13e2e545e
Иван Осягин <ayecantry@gmail.com>
2024-09-18 16:27:47 +0700

    слэши в адресе

--- app.py
+++ app.py
@@ -378,3 +378,11 @@ def gamingpc():
         'Who-Made-It': 'Ivan Osyagin', 
         'What-Have-I-Ate-Today': 'Pelmeni' 
     }
+
+@app.route("/lab2/a")
+def a():
+    return 'без слэша'
+
+@app.route("/lab2/a2")
+def a2():
+    return 'со слешем'
