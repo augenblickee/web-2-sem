6bec5164626bf07310f4adac1aaf03f96a77f9a7
Иван Осягин <ayecantry@gmail.com>
2024-10-12 16:55:43 +0700

    Заготовка к 3 лабе

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, url_for, redirect, render_template, abort, request ,redirect
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
+
 
 app = Flask(__name__)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
+
 
 @app.errorhandler(404)
 def not_found(err):
@@ -43,6 +47,7 @@ def start():
                 <ul>
                     <li><a href="/lab1">Первая лабораторная</a></li>
                     <li><a href="/lab2">Вторая лабораторная</a></li>
+                    <li><a href="/lab3">Третья лабораторная</a></li>
                 </ul>
             </main>
             <footer>
