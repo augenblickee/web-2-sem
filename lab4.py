0e1890d487c74a0db4b2973311041878022b2d29
Иван Осягин <ayecantry@gmail.com>
2024-10-29 21:04:37 +0700

    Доп задание

--- lab4.py
+++ lab4.py
@@ -105,7 +105,7 @@ def login():
         else:
             authorized = False
             login = ''
-        return render_template('lab4/login.html', authorized=authorized, login=login, name=name, last_login=last_login)
+        return render_template('lab4/login.html', authorized=authorized, login=login, name=name, last_login=last_login, users=users)
     
     login = request.form.get('login')
     password = request.form.get('password')
@@ -120,6 +120,8 @@ def login():
 
     if login == '':       
         error = 'Введите логин!'
+    elif login not in users:
+        error = 'Пользователь не найден!'
     elif password == '':       
         error = 'Введите пароль!'
 
@@ -195,3 +197,54 @@ def seeds():
             price = price - price * 0.1
     
     return render_template('/lab4/seeds.html', error=error, checked=checked, price=price, seed=seed, amount=amount, sale=sale)
+
+@lab4.route('/lab4/delete', methods=['POST'])
+def delete_user():
+    global users
+    login = session['login']
+    
+    users = [user for user in users if user['login'] != login]
+    
+    session.pop('login', None)
+    
+    return redirect('/lab4/login')
+
+
+@lab4.route('/lab4/change', methods=['GET', 'POST'])
+def change_user():
+    login = session['login']
+    user = next((user for user in users if user['login'] == login), None)
+
+    if request.method == 'POST':
+        new_name = request.form.get('name')
+        new_password = request.form.get('password')
+
+        if user:
+            user['name'] = new_name
+            user['password'] = new_password
+
+        return render_template('lab4/change.html', user=user)
+
+
+
+@lab4.route('/lab4/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        session.pop('login', None)
+        login = request.form.get('login')
+        password = request.form.get('password')
+        name = request.form.get('name')
+
+        # Проверка на уникальность логина
+        if any(user['login'] == login for user in users):
+            error = 'Пользователь с таким логином уже существует!'
+            return render_template('lab4/register.html', error=error)
+
+        # Добавление нового пользователя в массив users
+        users.append({'login': login, 'password': password, 'name': name, 'sex': 'not specified'})
+        
+        # Перенаправление на страницу логина после успешной регистрации
+        return redirect('/lab4/login')
+
+    # Рендеринг шаблона для GET-запроса
+    return render_template('lab4/register.html')
