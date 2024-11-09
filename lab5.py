71d77e0828a06f62286fc4acd59ed7f92cd2098e
Иван Осягин <ayecantry@gmail.com>
2024-11-09 20:51:42 +0700

    Заготовка к 5 лабораторной

---
+++ lab5.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template, request, redirect, session, make_response
+
+lab5 = Blueprint('lab5', __name__)
+
+@lab5.route('/lab5/')
+def lab():
+    return render_template('lab5/lab5.html')
+
+
