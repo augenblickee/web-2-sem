5be857ca1fbca83ab4b2cd03375debdc5c9b807f
Иван Осягин <ayecantry@gmail.com>
2024-11-10 18:15:22 +0700

    фикс 2 логин

--- lab5.py
+++ lab5.py
@@ -125,7 +125,7 @@ def create():
         cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
                     VALUES (%s, %s, %s);" , (login_id, title, article_text))
     else:
-        cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
+        cur.execute(f"INSERT INTO articles(login_id, title, article_text) \
                     VALUES (?, ?, ?);" , (login_id, title, article_text))
     
     db_close(conn, cur)
