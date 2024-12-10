6cc074897800983d543089845977c8ed4ffaeee3
Иван Осягин <ayecantry@gmail.com>
2024-12-10 18:50:29 +0700

    Проверка описания

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -55,6 +55,7 @@ function deleteFilm(id, title) {
 
 function showModal() {
     document.querySelector('div.modal').style.display = 'block';
+    document.getElementById('description-error').innerText = '';
 }
 
 function hideModal() {
@@ -71,6 +72,7 @@ function addFilm() {
     document.getElementById('title-ru').value = '';
     document.getElementById('year').value = '';
     document.getElementById('description').value = '';
+    document.getElementById('description-error').innerText = '';
     showModal();
 }
 
@@ -91,10 +93,18 @@ function sendFilm() {
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(film)
     })
-    .then(function() {
-        fillFilmList();
-        hideModal();
-    });
+    .then(function(resp) {
+        if(resp.ok) {
+            fillFilmList();
+            hideModal();
+            return {};
+        }
+        return resp.json();
+    })
+    .then(function(errors) {
+        if(errors.description)
+            document.getElementById('description-error').innerText = errors.description;
+    })
 }
 
 function editFilm(id) {
