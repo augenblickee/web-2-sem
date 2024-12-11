cb8ccc2c956352ec1d274faebcbcf77b46f44712
Иван Осягин <ayecantry@gmail.com>
2024-12-11 16:29:33 +0700

    Заготовка к 8 лабе

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 import os
 
@@ -21,6 +22,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 @app.errorhandler(404)
 def not_found(err):
@@ -65,6 +67,8 @@ def start():
                     <li><a href="/lab5">Пятая лабораторная</a></li>
                     <li><a href="/lab6">Шестая лабораторная</a></li>
                     <li><a href="/lab7">Седьмая лабораторная</a></li>
+                    <li><a href="/lab8">Восьмая лабораторная</a></li>
+
                 </ol>
             </main>
             <footer>
