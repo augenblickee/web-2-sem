545e8c63ba90d33d4611ebfde572d9ff3288301c
Иван Осягин <ayecantry@gmail.com>
2024-12-23 01:51:45 +0700

    Заготовка к 9 лабе

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 import os
 from os import path
@@ -36,6 +37,8 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
+
 
 
 if app.config['DB_TYPE'] == 'postgres':
@@ -97,6 +100,7 @@ def start():
                     <li><a href="/lab6">Шестая лабораторная</a></li>
                     <li><a href="/lab7">Седьмая лабораторная</a></li>
                     <li><a href="/lab8">Восьмая лабораторная</a></li>
+                    <li><a href="/lab9">Девятая лабораторная</a></li>
 
                 </ol>
             </main>
