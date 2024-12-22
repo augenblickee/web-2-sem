f8a54c917cc81d65fcaca57ca03cc541d851ad6d
Иван Осягин <ayecantry@gmail.com>
2024-12-23 00:14:54 +0700

    Создание статьи

--- lab8.py
+++ lab8.py
@@ -74,3 +74,24 @@ def article_list():
 def logout():
     logout_user()
     return redirect('/lab8/')
+
+
+@lab8.route('/lab8/create/', methods = ['GET', 'POST'])
+@login_required
+def create():
+        login_id = current_user.id
+        if request.method == 'GET':
+            return render_template('/lab8/create.html')
+        
+        title = request.form.get('title')
+        article_text = request.form.get('article_text')
+        is_public = request.form.get('is_public') == '1'
+
+        if not (title and article_text):
+            return render_template('/lab8/create.html', error='Введите текст и название статьи!')
+
+        new_article = articles(login_id = login_id, title = title, article_text = article_text, is_public = is_public)
+        db.session.add(new_article)
+        db.session.commit()
+
+        return redirect('/lab8/')
